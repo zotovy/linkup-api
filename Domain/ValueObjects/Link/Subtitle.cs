f022a16c106c6f8e410de7447c63151a69c95d40
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-04-24 21:02:47 +0500

    change TextValidator

--- Domain/ValueObjects/Link/Subtitle.cs
+++ Domain/ValueObjects/Link/Subtitle.cs
@@ -6,7 +6,7 @@ namespace Domain.ValueObjects.Link {
         public string Value { get; }
 
         public static readonly Regex Validator = new Regex(
-            "^([\x00-\x7F]|([\xC2-\xDF]|\xE0[\xA0-\xBF]|\xED[\x80-\x9F]|(|[\xE1-\xEC]|[\xEE-\xEF]|\xF0[\x90-\xBF]|\xF4[\x80-\x8F]|[\xF1-\xF3][\x80-\xBF])[\x80-\xBF])[\x80-\xBF])+$",
+            "^[^~,]*$",
             RegexOptions.Singleline | RegexOptions.Compiled
         );
 
